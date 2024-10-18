e09c9d8eba0c919937ae1ad77ffb07908ae9dd7a
Tigason509 <116956191+Tigason509@users.noreply.github.com>
2024-10-18 11:58:33 +0200

    Update qsn.js

--- js/qsn.js
+++ js/qsn.js
@@ -1,1 +1,13 @@
+const button = document.getElementById('toggle-button');
+
+button.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    
+    // Change le texte du bouton en fonction du mode
+    if (document.body.classList.contains('dark-mode')) {
+        button.textContent = 'Désactiver le mode sombre';
+    } else {
+        button.textContent = 'Activer le mode sombre';
+    }
+});
 
