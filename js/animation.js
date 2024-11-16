62168efde2f9280ce4318e35c444e83e8656d558
luccky0 <128980914+luccky0@users.noreply.github.com>
2024-11-16 23:15:58 +0100

    bouton gerer la taille text

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Créer un bouton
+ 
     const button = document.createElement("button");
     button.id = "toggle-dark-mode";
     button.textContent = "🌙"; // Utiliser un symbole de lune pour le mode sombre
@@ -19,5 +19,32 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             button.textContent = "🌙"; // Remettre à un symbole de lune
         }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Crée le bouton pour augmenter la taille du texte
+    const btnAugmenterText = document.createElement("button");
+    btnAugmenterText.id = "augmente-la-taille-text";
+    btnAugmenterText.textContent = "⬆️";
+
+    // Crée le bouton pour réduire la taille du texte
+    const btnReduireText = document.createElement("button");
+    btnReduireText.id = "reduire-la-taille-text";
+    btnReduireText.textContent = "⬇️";
+
+    // Ajoute les boutons au DOM
+    document.body.appendChild(btnAugmenterText);
+    document.body.appendChild(btnReduireText);
+
+    // Ajoute les événements pour augmenter et réduire la taille du texte
+    btnAugmenterText.addEventListener("click", () => {
+        document.body.classList.add("augmente-taille");
+        document.body.classList.remove("diminue-taille");
+    });
+
+    btnReduireText.addEventListener("click", () => {
+        document.body.classList.add("diminue-taille");
+        document.body.classList.remove("augmente-taille");
     });
 });
